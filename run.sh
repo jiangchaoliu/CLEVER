9f21ba67d16950f3986c1957ace2568f16524cc4
Jiangchao <liujiangchao1@gmail.com>
2020-02-03 00:03:22 +0800

    new script bug fixed

--- run.sh
+++ run.sh
@@ -1,7 +1,11 @@
 for i in {rfnn,rcnn}
-      for j in  {valid, wrong, fgsm1, fgsm05, cw, hop}
-		python3 collect_gradients.py --csvfile ${i}_${j}.csv  --data mnist --model_name ${i}_${j} --target_type 16 -n 100
-		sleep 20
-		python3 clever.py --stored_file ${i}_${j}.txt --untargeted ./lipschitz_mat/mnist_${i}_${j}/
-		sleep 20
+do
+    for j in  {valid, wrong, fgsm1, fgsm05, cw, hop}
+    do
+	python3 collect_gradients.py --csvfile ${i}_${j}.csv  --data mnist --model_name ${i}_${j} --target_type 16 -n 100
+	sleep 20
+	python3 clever.py --stored_file ${i}_${j}.txt --untargeted ./lipschitz_mat/mnist_${i}_${j}/
+	sleep 20
+     done
 
+done
